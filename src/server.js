8192868264a64173c7031ee051a90c1e3350d830
Giorgos Papanikolaou <g.pap2005@gmail.com>
2025-04-09 19:50:02 +0300

    transform sqlite to postgresql

--- src/server.js
+++ src/server.js
@@ -29,9 +29,11 @@ app.post("/api/login", async (req, res) => {
         .json({ message: "Username or password is missing" });
     }
 
-    const searchUsername = db
-      .prepare(`SELECT * FROM users WHERE username = ?`)
-      .get(username);
+    const { rows } = await db.query(`SELECT * FROM users WHERE username = $1`, [
+      username,
+    ]);
+    const searchUsername = rows[0];
+
     if (!searchUsername) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
@@ -63,19 +65,21 @@ app.post("/api/signup", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const check = db
-      .prepare(`SELECT * FROM users WHERE username = ?`)
-      .get(username);
+    const { rows } = await db.query(`SELECT * FROM users WHERE username = $1`, [
+      username,
+    ]);
+    const check = rows[0];
+
     if (check) {
       return res.status(409).json({ message: "User already exists" });
     }
 
     // Hash password asynchronously
     const hashedPassword = await bcrypt.hash(password, 10);
-    const insertion = db.prepare(
-      `INSERT INTO users (username, password) VALUES (?, ?)`
-    );
-    insertion.run(username, hashedPassword);
+    await db.query(`INSERT INTO users (username, password) VALUES ($1, $2)`, [
+      username,
+      hashedPassword,
+    ]);
 
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
@@ -85,38 +89,40 @@ app.post("/api/signup", async (req, res) => {
 });
 
 // Add course endpoint (only accessible to authenticated users)
-app.post("/api/course", (req, res) => {
+app.post("/api/course", async (req, res) => {
   const { course_id, name, description, link } = req.body;
 
   // Check if course already exists
-  const exists = db
-    .prepare(`SELECT * FROM courses WHERE course_id=?`)
-    .get(course_id);
+  const { rows } = await db.query(`SELECT * FROM courses WHERE course_id=$1`, [
+    course_id,
+  ]);
+  const exists = rows[0];
   if (exists) {
     return res.status(400).json({ message: "Course already exists" });
   }
 
   // Insert new course
   try {
-    const insertCourse = db.prepare(
-      `INSERT INTO courses (course_id, name, description, link) VALUES (?, ?, ?, ?)`
+    await db.query(
+      `INSERT INTO courses (course_id, name, description, link) VALUES ($1, $2, $3, $4)`,
+      [course_id, name, description, link]
     );
-    insertCourse.run(course_id, name, description, link);
     res.status(201).json({ message: "Course added successfully" });
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
-app.post("/api/deletecourse", (req, res) => {
+app.post("/api/deletecourse", async (req, res) => {
   const { course_id } = req.body;
 
   console.log("Received course_id:", course_id); // Log the received course_id
 
   // Check if the course exists
-  const course = db
-    .prepare(`SELECT * FROM courses WHERE course_id=?`)
-    .get(course_id);
+  const { rows } = await db.query(`SELECT * FROM courses WHERE course_id=$1`, [
+    course_id,
+  ]);
+  const course = rows[0];
 
   if (!course) {
     return res.status(404).json({ message: "Course not found" });
@@ -126,8 +132,7 @@ app.post("/api/deletecourse", (req, res) => {
 
   // Proceed with deleting the course
   try {
-    const deleteCourse = db.prepare(`DELETE FROM courses WHERE course_id=?`);
-    deleteCourse.run(course_id);
+    await db.query(`DELETE FROM courses WHERE course_id=$1`, [course_id]);
     res.status(200).json({ message: "Course deleted successfully" });
   } catch (err) {
     console.error("Error deleting course:", err); // Log the error
@@ -136,10 +141,10 @@ app.post("/api/deletecourse", (req, res) => {
 });
 
 // Get all courses endpoint
-app.get("/api/getcourses", (req, res) => {
+app.get("/api/getcourses", async (req, res) => {
   try {
-    const courses = db.prepare(`SELECT * FROM courses`).all();
-    res.json(courses);
+    const { rows } = await db.query(`SELECT * FROM courses`);
+    res.json(rows);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ message: "Internal server error" });
