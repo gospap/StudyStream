544364607be3e754497fbfba023c9d49a72c7bc2
Giorgos Papanikolaou <g.pap2005@gmail.com>
2025-04-12 17:36:06 +0300

    attempt #2

--- src/server.js
+++ src/server.js
@@ -182,6 +182,8 @@ app.post("/api/forgot-password", async (req, res) => {
 
   //send the email
   const resetLink = `https://studystream-zia9.onrender.com/reset-password?token=${token}`;
+  console.log(process.env.EMAIL_USER);
+  console.log(process.env.EMAIL_PASS);
 
   await sendEmail(
     email,
